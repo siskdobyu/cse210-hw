c1a4d955ac28b9b6f5303bb8d91db2e019f30717
siskdobyu <karinadolojan@gmail.com>
2025-09-16 02:26:27 +0800

    With changes for Week 1 Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int faveNumberInt = PromptUserNumber();
+        int square = SquareNumber(faveNumberInt);
+
+        DisplayResult(userName, square);
+
+
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string faveNumber = Console.ReadLine();
+        int faveNumberInt = int.Parse(faveNumber);
+        return faveNumberInt;
+    }
+
+    static int SquareNumber(int faveNumberInt)
+    {
+            
+        int square = faveNumberInt * faveNumberInt;
+        return square;
+    }
+
+    static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {square}");
+    }
+
+    
 }
