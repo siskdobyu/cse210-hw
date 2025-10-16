ed1c6889e65038a59d2eb31129b8cd40e1845ecf
siskdobyu <karinadolojan@gmail.com>
2025-10-17 05:10:49 +0800

    with changes for week 6

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create a list of shapes (polymorphism)
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 3),
+            new Rectangle("Blue", 4, 5),
+            new Circle("Green", 6),
+            
+        };
+
+        // Display color and area for each shape
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea():F2}");
+        }
     }
 }
